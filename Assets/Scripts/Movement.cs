b9312d639bd17b2313ef26f5b1edeed5816a66d8
Fábio Nogueira <fabiomiguel98@hotmail.com>
2019-03-08 12:58:36 +0000

    Initial commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    private void FixedUpdate()
+    {
+        offset.y = -0.55f;
+        Vector3 desiredPosition = target.position + offset;
+        transform.position = desiredPosition;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed;
+    private Rigidbody myRigidBody;
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+    private Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start(){
+        myRigidBody = GetComponent<Rigidbody>();
+        mainCamera = FindObjectOfType<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"),0f,Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput * moveSpeed;
+
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane oceanPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength; 
+
+        if(oceanPlane.Raycast(cameraRay,out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.red);
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+            //transform.LookAt(pointToLook);
+
+        }
+    }
+    private void FixedUpdate() {
+        myRigidBody.velocity = moveVelocity;
+    }
+}
